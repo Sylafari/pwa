4cb5eb57505740105c82e1651c8eebbc032d1df0
Quinn Cheng <sylafari@gmail.com>
2020-11-08 12:34:21 -0800

    Getting it to Run

--- my-vue-app/src/router/index.js
+++ my-vue-app/src/router/index.js
@@ -5,8 +5,8 @@ import PostsManager from '@/components/PostsManager'
 import Auth from '@okta/okta-vue'
 
 Vue.use(Auth, {
-  issuer: 'https://{yourOktaDomain}/oauth2/default',
-  client_id: '{clientId}',
+  issuer: 'https://dev-2389912.okta.com/oauth2/default',
+  client_id: '0oalmysmKm1mDvT2F5d5',
   redirect_uri: 'http://localhost:8080/implicit/callback',
   scope: 'openid profile email'
 })
